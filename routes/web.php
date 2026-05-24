f94dd49d137035827e0af126501dfda403fc83e7
Eslam-Nazer <eslam.nazer43@gmail.com>
2026-05-24 23:46:53 +0300

    feat: Define the resource cv controller in web route.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\CvController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('cv', CvController::class);
