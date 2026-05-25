5a033ada80d3cb2eab308a2f38e5cbc06c820fce
Eslam-Nazer <eslam.nazer43@gmail.com>
2026-05-25 03:06:21 +0300

    feat: New function download to convert the render html page (blade) to pdf file

--- app/Http/Controllers/CvController.php
+++ app/Http/Controllers/CvController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Storage;
+use Spatie\Browsershot\Browsershot;
 
 class CvController extends Controller
 {
@@ -14,5 +16,16 @@ class CvController extends Controller
             return json_decode(Storage::get("cv.json"), true);
         });
         return view("cv", compact("data"));
+    }
+
+    public function download(): Response
+    {
+        $html = view('cv', ['data' => json_decode(Storage::get("cv.json"), true)])->render();
+
+        $pdf = Browsershot::html($html)->pdf();
+
+        return response($pdf)
+            ->header('Content-Type', 'application/pdf')
+            ->header('Content-Disposition', 'attachment; filename="Eslam_CV.pdf"');
     }
 }
