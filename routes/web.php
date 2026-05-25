90863d7ca954d781b3372d9792841340d88a7ec5
Eslam-Nazer <eslam.nazer43@gmail.com>
2026-05-25 03:08:17 +0300

    feat: Init download cv as pdf in web routes

--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/cv/download', [CvController::class, 'download'])->name('cv.download');
 Route::resource('cv', CvController::class);
